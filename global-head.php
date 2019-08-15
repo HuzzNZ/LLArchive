530cf565ef1678289f08f163fd579fd7f5b7f040
Hunter <hunter.bis.12@outlook.com>
2019-08-16 01:25:14 +1200

    Completed Styling for Album Listing

--- global-head.php
+++ global-head.php
@@ -4,7 +4,6 @@
     <title>h/LoveLive! - Home</title>
 
     <meta name="description" content="Love Live! Music Archive">
-    <meta name="keywords" content="Love Live!, Muse, Aqours, PDP, Music, FLAC, Download">
     <meta name="author" content="Huzz#0009">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/love-live/css/main.css">
--- global-results.php
+++ global-results.php
@@ -35,7 +35,14 @@ do {
                     </div>
                 </div>
             </div>
-            <!--
+            <div class="album-listing">
+                <div class="list-header tl-grid">
+                    <div class="header-title">No.</div>
+                    <div class="header-title">Artist</div>
+                    <div class="header-title">Track Title</div>
+                    <div class="header-title">Duration</div>
+                    <div class="header-title">Download</div>
+                </div>
             <?php
             $album_id = $result['ID'];
             $song_sql = "SELECT * FROM `$album_id`";
@@ -45,26 +52,84 @@ do {
             do {
                 $song_id = $song_result["ID"];
                 ?>
-                <div class="song">
-                    <p class="song-id">
-                        <?php echo $song_result['ID'] ?>.
-                    </p>
-                    <h3 class="song-name">
-                        <?php echo $song_result['Name'] ?>
-                    </h3>
-                    <p class="song-duration"> Length:
-                        <?php echo $song_result['Length'] ?>
-                    </p>
-                    <a href="/love-live/media/uranohoshi/<?php echo $album_id ?>/<?php echo $song_id ?>.flac" download="<?php echo $song_result['Name']?>.flac">Download</a>
+                <div class="track tl-grid">
+                    <div class="track-id"><p>
+                            <?php echo str_pad(strval($song_id), 2, "0", STR_PAD_LEFT) ?>
+                        </p></div>
+                    <div class="track-artist">
+                        <div class="track-artist-wrapper">
+                            <p>
+                                <?php
+                                    if($song_result["Artist"]){
+                                        echo $song_result["Artist"];
+                                    } else {
+                                        echo $result["Artist"];
+                                    }
+                                ?>
+                            </p>
+                            <p class="jp">
+                                <?php
+                                if($song_result["Artist_JP"]){
+                                    echo $song_result["Artist_JP"];
+                                } else {
+                                    echo $result["Artist_JP"];
+                                }
+                                ?>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="track-title">
+                        <div class="track-title-wrapper">
+                            <p>
+                                <?php
+                                    echo $song_result['Name'];
+                                    if($song_result["Is_Instrumental"]){
+                                ?>
+                                        <span class="track-type instrumental">OFF VOCAL</span>
+                                <?php
+                                    }
+                                    if($song_result["Is_Radio"]){
+                                ?>
+                                        <span class="track-type radio">RADIO DRAMA</span>
+                                <?php
+                                    }
+                                ?>
+                            </p>
+                            <p class="jp">
+                                <?php echo $song_result['Name_JP'] ?>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="track-duration">
+                        <p>
+                            <?php
+                                $minutes = intdiv($song_result['Length'], 60);
+                                $seconds = $song_result["Length"] - ($minutes * 60);
+                                echo str_pad(strval($minutes), 2, "0", STR_PAD_LEFT);
+                                echo ":";
+                                echo str_pad(strval($seconds), 2, "0", STR_PAD_LEFT);
+                            ?>
+                        </p>
+                    </div>
+                    <div class="track-downloads">
+                        <div class="track-downloads-wrapper">
+                            <a class="download flac small" href="/love-live/media/uranohoshi/<?php echo $result['ID'] ?>/<?php echo $song_id ?>.flac" download="<?php echo $song_id ?>. <?php echo $song_result['Name'] ?>.flac">.flac</a>
+                            <a class="download mp3 small" href="/love-live/media/uranohoshi/<?php echo $result['ID'] ?>/<?php echo $song_id ?>.mp3" download="<?php echo $song_id ?>. <?php echo $song_result['Name'] ?>.mp3">.mp3</a>
+                        </div>
+                    </div>
                 </div>
                 <?php
             }
             while ($song_result = mysqli_fetch_assoc($song_query));
             ?>
-            -->
+            </div>
         </div>
     </div>
     <?php
 }
 while ($result = mysqli_fetch_assoc($query));
 ?>
+<!--
+<span class="track-type radio">RADIO DRAMA</span>
+<span class="track-type instrumental">OFF VOCAL</span>
+-->

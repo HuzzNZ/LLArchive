6e751a36a2363445f67d030ee3a3b466d498f188
Huzz <msgt.huzz@gmail.com>
2019-08-15 18:32:12 +1200

    Fix for UTF-8 Encoding

--- global-head.php
+++ global-head.php
@@ -1,5 +1,5 @@
 <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
     <title>h/LoveLive! - Home</title>
 
--- uranohoshi/db-config/db-connect.php
+++ uranohoshi/db-config/db-connect.php
@@ -2,6 +2,8 @@
 include "db-config-albums.php";
 $albums = mysqli_connect(servername_a, username_a, password_a, db_name_a);
 mysqli_set_charset($albums, "UTF-8");
+mysqli_query($albums, "SET NAMES 'utf8'");
 include "db-config-album-meta.php";
 $album_meta = mysqli_connect(servername_am, username_am, password_am, db_name_am);
 mysqli_set_charset($album_meta, "UTF-8");
+mysqli_query($album_meta, "SET NAMES 'utf8'");

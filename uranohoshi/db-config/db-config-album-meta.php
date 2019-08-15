e407a556ead3c5e3834c89b344ab4c2176f75875
Huzz <msgt.huzz@gmail.com>
2019-08-15 16:57:59 +1200

    Fixed DB names (again)

--- uranohoshi/db-config/db-config-album-meta.php
+++ uranohoshi/db-config/db-config-album-meta.php
@@ -5,4 +5,4 @@ ini_set("display_errors", "On");
 define("servername_am", "localhost");
 define("username_am", "root");
 define("password_am", "");
-define("db_name_am", "ura_album-meta");
+define("db_name_am", "ura_album_meta");

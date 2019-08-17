08f01aa3a092c0897bb07613c5a9c029070ee754
Hunter <msgt.huzz@gmail.com>
2019-08-18 00:48:39 +1200

    Fixed Index Page PHP

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE HTML>
 <head>
     <title>h/LoveLive! - Home</title>
-    <?php include "../global-head.php" ?>
+    <?php include "global-head.php" ?>
 </head>
 
 <body>
-<?php include "../global-nav.php" ?>
+<?php include "global-nav.php" ?>
 <div class="content-wrapper">
     <div class="side-search">
-        <?php include "../global-side-search.php"; ?>
+        <?php include "global-side-search.php"; ?>
     </div>
     <div class="main-content">
     </div>

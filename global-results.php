35532dd0427f8fe8311416631424b96ff46cbfb0
Hunter <msgt.huzz@gmail.com>
2019-08-18 02:19:58 +1200

    Fixed hover area overflow

--- global-results.php
+++ global-results.php
@@ -37,10 +37,12 @@ do {
                         <img class="download-icon" alt="Download Icon" src="/love-live/assets/download.png">
                         <a class="download flac <?php if ($result["Has_CUE"] != 1) {echo "locked";}?>" <?php if ($result["Has_CUE"]) {?>href="/love-live/media/uranohoshi/<?php echo $result['ID'] ?>/cd.flac" download="<?php echo $result['Catalog_Number'] ?>.flac"<?php } ?>>.flac</a>
                         <a class="download cue <?php if ($result["Has_CUE"] != 1) {echo "locked";}?>" <?php if ($result["Has_CUE"]) {?>href="/love-live/media/uranohoshi/<?php echo $result['ID'] ?>/cd.cue" download="<?php echo $result['Catalog_Number'] ?>.cue"<?php } ?>>.cue</a>
-                        <a class="copy-link" title="Copy Permalink" data-clipboard-text="<?php echo $base_url ?>/love-live/uranohoshi/album.php?id=<?php echo $result['ID'] ?>" href="javascript:void(0);">
-                            <img style="margin-left: 5px;" class="permalink-icon" alt="Link Icon" src="/love-live/assets/link.png">
+                        <div class="permalink-wrapper">
+                            <a class="copy-link" title="Copy Permalink" data-clipboard-text="<?php echo $base_url ?>/love-live/uranohoshi/album.php?id=<?php echo $result['ID'] ?>" href="javascript:void(0);">
+                                <img style="margin-left: 5px;" class="permalink-icon" alt="Link Icon" src="/love-live/assets/link.png">
+                            </a>
                             <p class="copy-tooltip">Copied to Clipboard!</p>
-                        </a>
+                        </div>
                     </div>
                 </div>
             </div>

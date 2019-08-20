ef3fcba1a04e64eb698e64a99fc4c72005e6d03c
Hunter <msgt.huzz@gmail.com>
2019-08-20 15:04:59 +1200

    Added Side Search HTML and CSS

--- global-side-search.php
+++ global-side-search.php
@@ -1,7 +1,88 @@
 <div class="search-wrapper">
     <div class="top-strip" id="search-top-strip"></div>
     <div class="search">
-        <h1>Search</h1>
+        <h1>Keyword Search</h1>
         <hr>
+        <form action="search.php" method="GET">
+            <h3 class="form-label">Song/Album Title</h3>
+            <label>
+                <input type="text">
+            </label>
+            <hr class="form-body-hr">
+            <h3 class="form-label">By Artist</h3>
+            <label>
+                <select>
+                    <option selected></option>
+                    <optgroup label="Main Units">
+                        <option>μ's</option>
+                        <option>Aqours</option>
+                        <option>Nijigasaki High School Idol Club</option>
+                    </optgroup>
+                    <optgroup label="Side Units">
+                        <option>A-RISE</option>
+                        <option>Saint Snow</option>
+                    </optgroup>
+                    <optgroup label="Subunits">
+                        <option>Printemps</option>
+                        <option>lily white</option>
+                        <option>BiBi</option>
+                        <option>CYaRon!</option>
+                        <option>AZALEA</option>
+                        <option>Guilty Kiss</option>
+                    </optgroup>
+                </select>
+            </label>
+            <h3 class="form-label">...or with Solo/Duo/Trios including</h3>
+            <label>
+                <select>
+                    <option selected></option>
+                    <optgroup label="μ's">
+                        <option>Honoka Kousaka</option>
+                        <option>Kotori Minami</option>
+                        <option>Umi Sonoda</option>
+                        <option>Hanayo Koizumi</option>
+                        <option>Rin Hoshizora</option>
+                        <option>Maki Nishikino</option>
+                        <option>Nico Yazawa</option>
+                        <option>Eli Ayase</option>
+                        <option>Nozomi Toujou</option>
+                    </optgroup>
+                    <optgroup label="Aqours">
+                        <option>Chika Takami</option>
+                        <option>You Watanabe</option>
+                        <option>Riko Sakurauchi</option>
+                        <option>Hanamaru Kunikida</option>
+                        <option>Yoshiko Tsushima</option>
+                        <option>Ruby Kurosawa</option>
+                        <option>Mari Ohara</option>
+                        <option>Dia Kurosawa</option>
+                        <option>Kanan Matsuura</option>
+                    </optgroup>
+                    <optgroup label="Nijigasaki High School Idol Club">
+                        <option>Ayumu Uehara</option>
+                        <option>Setsuna Yuki</option>
+                        <option>Ai Miyashita</option>
+                        <option>Shizuku Osaka</option>
+                        <option>Rina Tennoji</option>
+                        <option>Kasumi Nakasu</option>
+                        <option>Karin Asaka</option>
+                        <option>Emma Verde</option>
+                        <option>Kanata Konoe</option>
+                    </optgroup>
+                </select>
+            </label>
+            <hr class="form-body-hr">
+            <h3 class="form-label">Released after</h3>
+            <label class="date-label">
+                <input class="date-input" type="date" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}">
+            </label>
+            <h3 class="form-label">..and/or before</h3>
+            <label class="date-label">
+                <input class="date-input" type="date" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}">
+            </label>
+            <label class="submit">
+                <input type="submit" value="Go!">
+            </label>
+        </form>
     </div>
 </div>

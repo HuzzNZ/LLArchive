0ee225b0f3e02e18266bd833dbdd492b302ae732
Hunter <msgt.huzz@gmail.com>
2019-08-29 13:49:12 +1200

    Made Discord Integration Images Large Versions

--- global-album-head.php
+++ global-album-head.php
@@ -10,4 +10,4 @@
 ?>
 <meta content="h/LL - <?= $result['Name'] ?>" property="og:title">
 <meta content="Album by <?php echo $result['Artist'] ?>, released <?php $date  = date_create($result['Release_Date']); echo date_format($date,"M j Y"); ?>" property="og:description">
-<meta content="<?= $base_url ?>/love-live/media/<?= $generation ?>/<?= $result['ID'] ?>/cover-small.jpg" property="og:image">
+<meta content="<?= $base_url ?>/love-live/media/<?= $generation ?>/<?= $result['ID'] ?>/cover.jpg" property="og:image">

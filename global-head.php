6824c20f7c348db6e52291b86df5c400758c7e97
Hunter <msgt.huzz@gmail.com>
2019-09-05 01:06:55 +1200

    Updated Discord Embed's Domain from huzz.xyz to huzz.io

--- global-head.php
+++ global-head.php
@@ -6,7 +6,7 @@
 <meta name="author" content="Huzz#0009">
 <meta name="theme-color" content="#7289DA">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta content="huzz.xyz/love-live" property="og:site_name">
+<meta content="huzz.io/love-live" property="og:site_name">
 <link rel="stylesheet" type="text/css" href="/love-live/css/main.css">
 <link rel="icon" type="png/ico" href="/love-live/assets/favicon.png">
 

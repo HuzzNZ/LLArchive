c8482f35069c0bddb561d3f75e79df6f5e32ec76
Huzz <msgt.huzz@gmail.com>
2019-10-07 18:57:05 +1300

    Added Track Page for generic track finding

---
+++ t.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<head>
+    <?php
+        $album = $_GET["a"];
+        $track = $_GET["t"];
+        $file_type = $_GET["f"];
+
+        if (!$file_type){
+            $file_type = "mp3";
+        }
+
+        $gen = $album[0];
+        $album_id = substr($album, 1);
+        $generation = "";
+        if ($gen === "o") {
+            $generation = "otonokizaka";
+        } elseif ($gen === "u") {
+            $generation = "uranohoshi";
+        } elseif ($gen === "n") {
+            $generation = "nijigasaki";
+        }
+    ?>
+    <title>h/LoveLive! - Track</title>
+    <?php include "global-head.php" ?>
+</head>
+
+<body>
+<?php include "global-nav.php" ?>
+<div class="content-wrapper">
+    <div class="side-search">
+        <?php include "global-side-search.php"; ?>
+    </div>
+    <div class="main-content text-only">
+        <audio controls src="<?= $base_url ?>/love-live/media/<?= $generation ?>/<?= $album_id ?>/<?= $track ?>.<?= $file_type?>">
+        </audio>
+    </div>
+</div>
+</body>
+

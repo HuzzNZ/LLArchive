fcd8d1273eacdd97aed2de949befda491902ecf8
Huzz <msgt.huzz@gmail.com>
2019-11-20 22:44:34 +1300

    Chaanged Album Embed theme color

--- global-album-head.php
+++ global-album-head.php
@@ -8,6 +8,7 @@
         $generation = "nijigasaki";
     }
 ?>
+<meta name="theme-color" content="#ffff00">
 <meta content="💿 <?= $result['Name'] ?>" property="og:title">
 <meta content="Album by <?php echo str_replace(",", ", ", $result['Artist']) ?>, released <?php $date  = date_create($result['Release_Date']); echo date_format($date,"M j, Y"); ?>" property="og:description">
 <meta content="<?= $base_url ?>/love-live/media/<?= $generation ?>/<?= $result['ID'] ?>/cover-small.jpg" property="og:image">

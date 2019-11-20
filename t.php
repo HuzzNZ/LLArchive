b001084708328918a24b216957cacaa4acd6b2f5
Huzz <msgt.huzz@gmail.com>
2019-11-20 22:46:35 +1300

    Testing Markdown on Discord Embed

--- t.php
+++ t.php
@@ -56,7 +56,7 @@
     <?php include "global-head.php" ?>
     <meta name="theme-color" content="#00e37d">
     <meta content="🎵 <?= $track_result['Name'] ?>" property="og:title">
-    <meta content="Song by <?= str_replace(",", ", ", $artist)?>, track <?= $track_result["ID"] ?> of the <?= $album_result["Name"] ?> album" property="og:description">
+    <meta content="Song by <?= str_replace(",", ", ", $artist)?> - **Track <?= $track_result["ID"] ?>** of the <?= $album_result["Name"] ?> album" property="og:description">
     <meta content="<?= $base_url ?>/love-live/media/<?= $generation ?>/<?= $album_result['ID'] ?>/cover-small.jpg" property="og:image">
 </head>
 <body>

4fefc3bca2d6e04b89e4dfffefb174bfb0ce8758
Huzz <msgt.huzz@gmail.com>
2019-11-20 23:09:40 +1300

    Edited Track Embeds for consistency

--- global-album-head.php
+++ global-album-head.php
@@ -10,5 +10,5 @@
 ?>
 <meta name="theme-color" content="#ffff00">
 <meta content="💿 │ <?= $result['Name'] ?>" property="og:title">
-<meta content="Album by <?php echo str_replace(",", ", ", $result['Artist']) ?>, released <?php $date  = date_create($result['Release_Date']); echo date_format($date,"M j, Y"); ?>" property="og:description">
+<meta content="Album by <?php echo str_replace(",", ", ", $result['Artist']) ?> - Released <?php $date  = date_create($result['Release_Date']); echo date_format($date,"M j, Y"); ?>" property="og:description">
 <meta content="<?= $base_url ?>/love-live/media/<?= $generation ?>/<?= $result['ID'] ?>/cover-small.jpg" property="og:image">

e3f55052c1a64475b1ca0a338acee20db63633a9
Huzz <msgt.huzz@gmail.com>
2019-12-13 22:06:14 +1300

    Mobile CSS Chapter 20

--- global-results.php
+++ global-results.php
@@ -159,7 +159,7 @@ for ($i = 0; $i < $count; $i++) {
                         </div>
                         <div class="track-duration">
                             <p class="track-duration-text">
-                                <span class="mobile-only">Duration: </span><?php
+                                <?php
                                 $minutes = intdiv($song_result['Length'], 60);
                                 $seconds = $song_result["Length"] - ($minutes * 60);
                                 echo str_pad(strval($minutes), 2, "0", STR_PAD_LEFT);

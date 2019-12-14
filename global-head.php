ce6bec1637d075fada832326b03ae3f443d5770e
Huzz <msgt.huzz@gmail.com>
2019-12-14 20:55:30 +1300

    Mobile CSS Chapter 55

--- global-head.php
+++ global-head.php
@@ -33,4 +33,12 @@
     $rl_before = "";
 
     $catalog = "";
+
+    function human_filesize($bytes, $dec = 2)
+    {
+        $size   = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+        $factor = floor((strlen($bytes) - 1) / 3);
+
+        return sprintf("%.{$dec}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+    }
 ?>

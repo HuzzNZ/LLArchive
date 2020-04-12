94e77cafc27145f7be222baf3174d66d5f6c1a42
Huzz <msgt.huzz@gmail.com>
2020-04-13 03:35:13 +1200

    Added TBA Date support for Discord

--- global-album-head.php
+++ global-album-head.php
@@ -10,5 +10,5 @@
 ?>
 <meta name="theme-color" content="#ffff00">
 <meta content="💿 │ <?php echo str_replace('"', "&quot;", $result['Name']); ?>" property="og:title">
-<meta content="Album by <?php echo str_replace(",", ", ", $result['Artist']) ?> - <?php if (date_create($result['Release_Date']) < time()) {echo "Released";} else {echo "Releasing";} ?> <?php $date  = date_create($result['Release_Date']); echo date_format($date,"M j, Y"); ?>" property="og:description">
+<meta content="Album by <?php echo str_replace(",", ", ", $result['Artist']) ?> - <?php if ($result['Release_Date']){if (date_create($result['Release_Date']) < time()) {echo "Released";} else {echo "Releasing";}} else {echo "Release date TBA";} ?> <?php if ($result['Release_Date']){$date = date_create($result['Release_Date']); echo date_format($date,"M j, Y");} ?>" property="og:description">
 <meta content="<?= $base_url ?>/love-live/media/<?= $generation ?>/<?= $result['ID'] ?>/cover.jpg" property="og:image">

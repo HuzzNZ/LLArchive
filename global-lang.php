68064fece9a5067e04e70c974995ba4142adf8e9
Huzz <msgt.huzz@gmail.com>
2020-05-27 16:34:40 +1200

    Fix include bug

--- global-lang.php
+++ global-lang.php
@@ -2,6 +2,8 @@
 $supported_langs = array('en', 'ja');
 $languages = explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);
 
+define("HOME_DIR", explode("/", $_SERVER["SCRIPT_NAME"])[1]);
+
 foreach($languages as $lang) {
     if (in_array($lang, $supported_langs)) {
         // Set the page locale to the first supported language found
@@ -12,10 +14,10 @@ foreach($languages as $lang) {
 
 if (LANG == "en") {
     $path = $_SERVER['DOCUMENT_ROOT'];
-    $path .= "/langs/lang_en.php";
+    $path .= "/".HOME_DIR."/langs/lang_en.php";
 } elseif (LANG == "jp") {
     $path = $_SERVER['DOCUMENT_ROOT'];
-    $path .= "/langs/lang_jp.php";
+    $path .= "/".HOME_DIR."/langs/lang_jp.php";
 }
 
 include $path;
